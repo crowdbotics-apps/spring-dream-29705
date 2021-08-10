034d22e956fd77c830dcfc934bc3ddde8a894aa6
Crowdbotics <team@crowdbotics.com>
2021-08-10 23:10:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "spring_dream_29705Identifier",
+  url: "https://spring_dream_29705Identifier.botics.co",
+  api: "https://spring_dream_29705Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
